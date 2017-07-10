f6fddd8404a781a6d3b28928fc5c1727ef45084a
Richard Suller <rsuller@companieshouse.gov.uk>
2017-07-10 15:54:49 +0100

    Removed unused implemented class

--- src/test/java/uk/gov/companieshouse/taf/StepsConfig.java
+++ src/test/java/uk/gov/companieshouse/taf/StepsConfig.java
@@ -1,11 +1,10 @@
 package uk.gov.companieshouse.taf;
 
-import cucumber.api.java8.En;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ContextConfiguration;
 import uk.gov.companieshouse.taf.config.AppConfig;
 
 @ContextConfiguration(classes = {AppConfig.class})
 @DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_CLASS)
-public class StepsConfig implements En {
+public class StepsConfig {
 }
