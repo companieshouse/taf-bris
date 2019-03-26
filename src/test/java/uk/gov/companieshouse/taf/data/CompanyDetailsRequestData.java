1e5532b61880ed214898c3bcb108d82b760fb858
Karen Brown <kbrown@companieshouse.gov.uk>
2019-03-26 16:06:58 +0000

    changes files for v2 response wip

--- src/test/java/uk/gov/companieshouse/taf/data/CompanyDetailsRequestData.java
+++ src/test/java/uk/gov/companieshouse/taf/data/CompanyDetailsRequestData.java
@@ -2,18 +2,19 @@ package uk.gov.companieshouse.taf.data;
 
 
 import eu.europa.ec.bris.jaxb.br.company.details.response.v1_4.BRCompanyDetailsResponse;
+import eu.europa.ec.digit.message.container.jaxb.v1_0.MessageContainer;
 import org.springframework.stereotype.Component;
 
 @Component
 public class CompanyDetailsRequestData extends RequestData {
 
-    private BRCompanyDetailsResponse companyDetailsResponse;
+    private MessageContainer companyDetailsResponse;
 
-    public BRCompanyDetailsResponse getCompanyDetailsResponse() {
+    public MessageContainer getCompanyDetailsResponse() {
         return companyDetailsResponse;
     }
 
-    public void setCompanyDetailsResponse(BRCompanyDetailsResponse companyDetailsResponse) {
+    public void setCompanyDetailsResponse(MessageContainer companyDetailsResponse) {
         this.companyDetailsResponse = companyDetailsResponse;
     }
 
